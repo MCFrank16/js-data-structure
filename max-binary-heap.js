3c112a206ab922123dbe05c931efac1cf1367730
MCFrank16 <mecfrank16@gmail.com>
2020-07-10 19:23:34 +0200

    extract the maximum values of the heap

--- max-binary-heap.js
+++ max-binary-heap.js
@@ -21,11 +21,66 @@ class MaxBinaryHeap{
             elIndex = parentIdx; 
         }
     }
+
+    extractMax(){
+       const max = this.values[0];
+       const end = this.values.pop();
+       if(this.values.length > 0){
+        this.values[0] = end;
+        this.bubbleDown();
+       }
+       return max;
+
+    }
+
+    bubbleDown(){ 
+        let idx = 0;
+        const length = this.values.length;
+        const el = this.values[0];
+
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild > el) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild < length) || 
+                    (swap !== null && rightChild > leftChild)
+                    ) {
+                        swap = rightChildIdx;
+                    }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = el;
+            idx = swap;
+        }
+
+    }
 }
 
 let heap = new MaxBinaryHeap();
 
 heap.insert(55)
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+
 
 console.log(heap.values);
 
